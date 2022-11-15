d097dd36af8a551f37389e6f6aee86f1856cda60
Ainebyoona Dickson <92643336+Aine-dickson@users.noreply.github.com>
2022-11-15 18:23:18 +0300

    Functionality improvement

--- To-do/script.js
+++ To-do/script.js
@@ -1,38 +1,55 @@
+let taskTitle;
+let taskContent;
+
 $("document").ready(() => {
+    $("#add").click(() => {
+        $(".edits").removeClass("hidden")
+    })
+// Functionality of adding an edited task to the display window
     $("#push").click(() => {
+        $(".edits").addClass("hidden")
+        taskTitle = $(".title-text").text()
+        taskContent = $(".task-body").text()
         let task_template = document.createElement("div")
         task_template.setAttribute("class", "task")
         task_template.innerHTML = `
-        <span class="task-title">Aine</span>
+        <span class="task-title">${taskTitle}</span>
         <div class="task-actions">
-            <span class="delete">D</span>
-            <span class="edit">E</span>
+            <span class="delete"><i class="fas fa-delete"></i></span>
+            <span class="edit"><i class="fas fa-pencil"></i></span>
         </div>
         </div>`
         $(".task-display").prepend(task_template)
+        $(".title-text").text("")
+        $(".task-body").text("")
     })
+// Provision for writing text ability
     $(".title-text").click(() => {
         $("#title").focus()
         $("#title").focus(() => {
             if($(".title-text").val() != " "){
                 $(".title-text").css({"text-decoration":"underline wavy green 0.05rem"})
-                console.log("red")
             } else {
                 $(".title-text").css("color", "black")
-                console.log("black")
             }
         })
         keyEntry("title")
     })
+
     $("#title").focusout(() => {
         $("#title").val("")
     })
+
     $(".task-body").click(() => {
         $("#task-body").focus()
         $("#task-body").focus(() => {
             console.log("Body focused!")
         })
         keyEntry("body")
+    })
+
+    $("#task-body").focusout(() => {
+        $("#task-body").val("")
     })
 })
 
